46598f212e78f127388777bc9d23fcb61a1ead96
Balázs Marcell <KoegLike@proton.me>
2026-03-14 19:34:00 +0200

    moved renders to different folder

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to HAckxspansion</h1>
+<p>this is a test</p>
+<h1>Test Heading</h1>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
