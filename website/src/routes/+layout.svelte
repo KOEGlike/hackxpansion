c37b24416fc6e97cd1e069986936dfe285616fca
Balázs Marcell <KoegLike@proton.me>
2026-03-16 20:53:41 +0200

    added favicon

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { asset } from '$app/paths';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href={asset('/favicon/apple-touch-icon.png')} />
+	<link rel="icon" type="image/png" sizes="32x32" href={asset('/favicon/favicon-32x32.png')} />
+	<link rel="icon" type="image/png" sizes="16x16" href={asset('/favicon/favicon-16x16.png')} />
+</svelte:head>
 {@render children()}
