69c5ccd9c642f959916d41ec860fe5d6e3bb3799
Balázs Marcell <KoegLike@proton.me>
2026-03-19 12:20:19 +0200

    adjusted padding for header section on simple and main pages

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -78,13 +78,13 @@
 					class="flex h-full w-full flex-col items-center justify-between gap-0"
 					transition:fade={{ duration: 100 }}
 				>
-					<div class="flex flex-col items-center gap-0 pt-[35vh]">
+					<div class="flex flex-col items-center gap-0 pt-[32vh]">
 						<h1 class="text-5xl font-bold text-slate-700 sm:text-7xl">Hackxpansion</h1>
 						<h2 class="w-80 text-center text-xl font-normal text-slate-500 sm:w-100 sm:text-2xl">
 							Make 4 expansion cards, get a custom console to use them in!
 						</h2>
 					</div>
-					<div class="mb-20 flex flex-col items-center justify-center">
+					<div class="mb-[6vh] flex flex-col items-center justify-center">
 						<a
 							href="https://meko.fillout.com/hackxpansion"
 							target="_blank"
--- website/src/routes/simple/+page.svelte
+++ website/src/routes/simple/+page.svelte
@@ -14,13 +14,13 @@
 		class="flex h-screen w-screen flex-col items-center justify-between gap-0 bg-cover bg-center"
 		style:background-image="url({asset(`/renders/0000.webp`)})"
 	>
-		<div class="flex flex-col items-center justify-center gap-0 pt-[35vh]">
+		<div class="flex flex-col items-center justify-center gap-0 pt-[32vh]">
 			<h1 class="text-5xl font-bold text-slate-700 sm:text-7xl">Hackxpansion</h1>
 			<h2 class="w-80 text-center text-xl font-normal text-slate-500 sm:w-100 sm:text-2xl">
 				Make 4 expansion cards, get a custom console to use them in!
 			</h2>
 		</div>
-		<div class="mb-20 flex flex-col items-center justify-center">
+		<div class="mb-[6vh] flex flex-col items-center justify-center">
 			<a
 				href="https://meko.fillout.com/hackxpansion"
 				target="_blank"
