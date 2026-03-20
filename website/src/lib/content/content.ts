003d5be390da6718d1725e019545c6fcc94e6a3a
Balázs Marcell <KoegLike@proton.me>
2026-03-20 19:50:33 +0200

    Edited docs and content

--- website/src/lib/content/content.ts
+++ website/src/lib/content/content.ts
@@ -6,16 +6,16 @@ export const landingContent = {
 	steps: [
 		{
 			title: 'Step 1',
-			description: 'Design your card! Make a weird input card! Or add a radio! Go wild!'
+			description: 'Design your card! Make a weird input card, or add a radio! Go wild!'
 		},
 		{
 			title: 'Step 2',
 			description:
-				"Code a driver in Rust! Don't worry, it won't be hard! We'll help you through it!"
+				"Code a driver in Rust! Don't worry, it won't be hard, we'll help you through it!"
 		},
 		{
 			title: 'Step 3',
-			description: "Repeat 3 times! Make modules that complement each other! Or don't! Be creative!"
+			description: "Repeat 3 times! Make modules that complement each other, or don't! Be creative!"
 		},
 		{ title: 'Step 4', description: 'Submit and get the console!' }
 	],
--- website/src/routes/docs/+page.svelte
+++ website/src/routes/docs/+page.svelte
@@ -4,16 +4,17 @@ This is an event run by Hack Club, a global community of high school hackers.
 The goal of this event is to make 4 expansion cards, write drivers for them, and then get a custom console
 to use them in. And maybe even get other people's modules.
 
-<h3>But I'm a beginner and don't know hardware</h3>
-No problem! We have guides and resource that will help you make your first module, even your first PCB!
-
-<h3>How powerful will the console be?</h3>
-The console will use a RP2354B MCU as it's main processor, it is the generation of the chip used in
-<a href="https://sprig.hackclub.com/">Sprig</a>. It can handle a lot of stuff, and it's really easy
-to program for. You could make a co-processor if you yearned for more power!!
-
 <h3>Where do I get started?</h3>
 <a href="https://slack.hackclub.com/">Join</a> the Hack Club Slack and check out the
-<a href="https://hackclub.enterprise.slack.com/archives/C0A9B4152BY"> #hackxpansion</a> channel. We have
-a bunch of people who are eger to help! Also check out our getting started guide and tutorials to learn
-more about making expansion cards and programming drivers/apps.
+<a href="https://hackclub.enterprise.slack.com/archives/C0A9B4152BY">#hackxpansion</a> channel. We
+have a bunch of people who are eger to help! Also check out our getting started guides and tutorials
+to learn more about making your first expansion card and programming drivers/apps.
+
+<h3>But I'm a beginner and don't know hardware</h3>
+No problem! We have guides and resources that will help you make your first module, even your first PCB
+
+<h3>How powerful will the console be?</h3>
+The console will use a RP2354B MCU as it's main processor, it is the next generation of the chip used
+in
+<a href="https://sprig.hackclub.com/">Sprig</a>. It can handle a lot of stuff, and it's really easy
+to program for. You could make a co-processor if you yearned for more power!!
