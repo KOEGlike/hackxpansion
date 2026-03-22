efcdb6df8c7083fcc567e8b737bc2c7dc5c99384
Balázs Marcell <KoegLike@proton.me>
2026-03-22 23:33:32 +0200

    updated docs

--- website/src/routes/docs/+page.svelte
+++ website/src/routes/docs/+page.svelte
@@ -1,14 +1,17 @@
 <h3>What is this all about?</h3>
 This is an event run by Hack Club, a global community of high school hackers.
 <br />
-The goal of this event is to make 4 expansion cards, write drivers for them, and then get a custom console
-to use them in. And maybe even get other people's modules.
+The goal of this event is to make 4 expansion cards, write drivers for them, get funding, and then get
+a custom console to use them in. And maybe even get other people's modules.
 
 <h3>Where do I get started?</h3>
 <a href="https://slack.hackclub.com/">Join</a> the Hack Club Slack and check out the
 <a href="https://hackclub.enterprise.slack.com/archives/C0A9B4152BY">#hackxpansion</a> channel. We
-have a bunch of people who are eger to help! Also check out our getting started guides and tutorials
-to learn more about making your first expansion card and programming drivers/apps.
+have a bunch of people who are eager to help! Also check out our getting started guides and
+tutorials to learn more about making your first expansion card and programming drivers/apps.
+<br />
+You will need to track you time by writing a journal/making a time-lapse/streaming your work. This is
+a requirement to get the free console at the end, and it's also a great way to
 
 <h3>But I'm a beginner and don't know hardware</h3>
 No problem! We have guides and resources that will help you make your first module, even your first PCB
