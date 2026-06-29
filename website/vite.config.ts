908e9862de990c8972f951f95152f3b4ccc490f8
KOEGlike <koeglike@proton.me>
2026-06-29 11:23:36 +0300

    updated packages

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,8 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
-import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), enhancedImages(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()]
 });
