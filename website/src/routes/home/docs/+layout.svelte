21daf09e15a7a351c4f453a373986b46099f62d6
KOEGlike <koeglike@proton.me>
2026-08-01 18:42:12 +0300

    added custom `code` to md parsing

--- website/src/routes/home/docs/+layout.svelte
+++ website/src/routes/home/docs/+layout.svelte
@@ -7,3 +7,19 @@
 >
 	{@render children()}
 </main>
+
+<style>
+	:global(.prose :not(pre) > code) {
+		border: 1px solid rgb(148 163 184);
+		border-radius: 0.25rem;
+		background: rgb(226 232 240);
+		padding: 0.1rem 0.35rem;
+		font-weight: 400;
+		color: rgb(30 41 59);
+	}
+
+	:global(.prose :not(pre) > code::before),
+	:global(.prose :not(pre) > code::after) {
+		content: none;
+	}
+</style>
